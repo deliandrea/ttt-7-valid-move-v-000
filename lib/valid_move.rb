8d81acabb8f480a2b97948bbf4fef3aa7d742c79
Delia nunez <ndeliandrea@gmail.com>
2018-07-29 23:21:30 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move(position_taken)
-    if position_taken?
+    if position_taken == true
         execute something
     else
         execute something else
